523704a9b8c9b096761c56949c8a55c7ac94085b
João Victor <joaocarvoli@hotmail.com>
2023-08-28 23:38:15 -0300

    updating coding with exceptions catch

--- 02.work/Helper.kt
+++ 02.work/Helper.kt
@@ -33,17 +33,21 @@ object Helper {
     fun copyFileByBlockSize(basePath: String = BASE_PATH,
                               sourcePath: String, destinationPath: String, blockSize: Int = 8192
     ){
-        val sourceFile = File(basePath + sourcePath)
-        val destinationFile = File(basePath + destinationPath)
+        try {
+            val sourceFile = File(basePath + sourcePath)
+            val destinationFile = File(basePath + destinationPath)
 
-        FileInputStream(sourceFile).use { fileInputStream ->
-            FileOutputStream(destinationFile).use { fileOutputStream ->
-                val buffer = ByteArray(blockSize)
-                var len: Int
-                while (fileInputStream.read(buffer).also { len = it } != -1) {
-                    fileOutputStream.write(buffer, 0, len)
+            FileInputStream(sourceFile).use { fileInputStream ->
+                FileOutputStream(destinationFile).use { fileOutputStream ->
+                    val buffer = ByteArray(blockSize)
+                    var len: Int
+                    while (fileInputStream.read(buffer).also { len = it } != -1) {
+                        fileOutputStream.write(buffer, 0, len)
+                    }
                 }
             }
+        } catch (exception: Exception){
+            println(exception)
         }
     }
 
@@ -54,21 +58,25 @@ object Helper {
     fun readAsIsoAndConvertToUTF(basePath: String = BASE_PATH,
                                  sourcePath: String, destinationPath: String, blockSize: Int = 1024)
     {
-        val sourceFile = File(basePath + sourcePath)
-        val destinationFile = File(basePath + destinationPath)
+        try {
+            val sourceFile = File(basePath + sourcePath)
+            val destinationFile = File(basePath + destinationPath)
 
-        FileInputStream(sourceFile).use { fileInputStream ->
-            InputStreamReader(fileInputStream, Charset.forName("ISO-8859-1")).use { inputStreamReader ->
-                FileOutputStream(destinationFile).use { fileOutputStream ->
-                    OutputStreamWriter(fileOutputStream, Charset.forName("UTF-8")).use { outputStreamReader ->
-                        val buffer = CharArray(blockSize)
-                        var charsRead: Int
-                        while (inputStreamReader.read(buffer).also { charsRead = it } != -1) {
-                            outputStreamReader.write(buffer, 0, charsRead)
+            FileInputStream(sourceFile).use { fileInputStream ->
+                InputStreamReader(fileInputStream, Charset.forName("ISO-8859-1")).use { inputStreamReader ->
+                    FileOutputStream(destinationFile).use { fileOutputStream ->
+                        OutputStreamWriter(fileOutputStream, Charset.forName("UTF-8")).use { outputStreamReader ->
+                            val buffer = CharArray(blockSize)
+                            var charsRead: Int
+                            while (inputStreamReader.read(buffer).also { charsRead = it } != -1) {
+                                outputStreamReader.write(buffer, 0, charsRead)
+                            }
                         }
                     }
                 }
             }
+        } catch (exception: Exception){
+            println(exception)
         }
     }
 
@@ -77,21 +85,31 @@ object Helper {
      */
     fun readInputUntilACharacterAndSave(basePath: String = BASE_PATH,
                                  destinationPath: String, stopCharacter: String = "END"){
-        val reader = Scanner(System.`in`)
+        try {
+            val reader = Scanner(System.`in`)
 
-        while(true){
-            val line = reader.nextLine()
-            if(line == stopCharacter) break
-            appendLineToFile(filePath = basePath + destinationPath,
-                content = line)
+            while (true) {
+                val line = reader.nextLine()
+                if (line == stopCharacter) break
+                appendLineToFile(
+                    filePath = basePath + destinationPath,
+                    content = line
+                )
+            }
+        } catch (exception: Exception){
+            println(exception)
         }
     }
 
     private fun appendLineToFile(filePath: String, content: String){
-        val file = File(filePath)
+        try {
+            val file = File(filePath)
 
-        FileOutputStream(file, true).use { fileOutputStream ->
-            fileOutputStream.write("$content\n".toByteArray())
+            FileOutputStream(file, true).use { fileOutputStream ->
+                fileOutputStream.write("$content\n".toByteArray())
+            }
+        } catch (exception: Exception){
+            println(exception)
         }
     }
 }
